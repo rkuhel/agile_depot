6abdc2aeb97c22e1be9d41e6f9a15731a095a2bd
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-27 15:37:29 -0400

    first commit products can be created, edited, and deleted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'a72fb453210dab8a03b825d4719b23a6304227e04aa357493c73ebe27b68ae1eeb104f17b4ad068a724f2e4ddb6bd30fe618ccf7cbe07807f164ffec3e78c11d'
