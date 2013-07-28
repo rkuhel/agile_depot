bca8850e7121e80a252274fc2de734c2b702728b
Ryan Kuhel <ryan.kuhel@gmail.com>
2013-07-28 13:38:12 -0400

    shopping cart added to store

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728161923) do
+ActiveRecord::Schema.define(version: 20130728170552) do
 
   create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
